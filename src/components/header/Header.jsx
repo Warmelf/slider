bb482a1580ae21da72c25f37cc26827436372ce2
Warmelf <warmelf17@gmail.com>
2023-06-29 12:48:03 +0300

    add header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import "./Header.css";
+import Logo from "../../assets/logo.svg";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header__container">
+        <a href="/">
+          <img className="header__logo" src={Logo} alt="CIRCEYA" />
+        </a>
+        <a className="header__link" href="tel:+74954954954">
+          +7 (495) 495-49-54
+        </a>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import './fonts/Manrope/Manrope-Medium.ttf';
+import './fonts/Orchidea/OrchideaPro-Medium.ttf';
+import './fonts/Roboto/Roboto-Regular.ttf';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
